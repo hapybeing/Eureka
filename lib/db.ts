a8b6454a4abeaa7c42d7edca1ee8d899c9cab37e
hapybeing <gaurangk.inbox@gmail.com>
2026-03-04 17:30:14 +0530

    Create db.ts

---
+++ lib/db.ts
@@ -0,0 +1,10 @@
+/**
+ * Database placeholder module.
+ *
+ * Prisma client initialization should be enabled after running:
+ *   pnpm prisma generate
+ * in an environment that can download Prisma engines.
+ */
+export const db = {
+  isConfigured: false,
+};
